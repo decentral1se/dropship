b6a38f38219737374802b8b33f9fcf43a9654041
Luke Murphy <lukewm@riseup.net>
2020-10-12 13:54:51 +0200

    Thread args/kwargs on template

--- dropship/ui_templates.py
+++ dropship/ui_templates.py
@@ -19,8 +19,8 @@ class PendingTransferRow(Gtk.ListBoxRow):
     transferCodeButton = Gtk.Template.Child()
     cancelTransfer = Gtk.Template.Child()
 
-    def __init__(self, fileName, transferCode):
-        super(PendingTransferRow, self).__init__()
+    def __init__(self, fileName, transferCode, *args, **kwargs):
+        super(PendingTransferRow, self).__init__(*args, **kwargs)
         self.fileNameLabel.set_text(fileName)
         self.transferCodeButton.set_label(transferCode)
 
